390ea336c82aeddc1de526de923c5c386c37346c
Jayasurya_JS7 <my600yll@gmail.com>
2024-11-26 10:39:08 +0530

    calibration and flappy game

---
+++ Assets/FlappyBirdStyleAssets/Scripts/BirdControl.cs
@@ -0,0 +1,162 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BirdControl : MonoBehaviour
+{
+    private bool isDead = false;
+    public static Rigidbody2D rb2d;
+    Animator anime;
+    // player controls
+
+    public static float playSize;
+    public static int FlipAngle = 1;
+    public static float tempRobot, tempBird;
+    public bool set = false;
+
+
+    int totalLife = 5;
+    int currentLife = 0;
+    bool columnHit;
+    public Image life;
+
+    public float spriteBlinkingTimer = 0.0f;
+    public float spriteBlinkingMiniDuration = 0.1f;
+    public float spriteBlinkingTotalTimer = 0.0f;
+    public float spriteBlinkingTotalDuration = 2f;
+    public bool startBlinking = false;
+
+    float startTime;
+    float endTime;
+
+    float targetAngle;
+
+    public FlappyGameControl FGC;
+    void Start()
+    {
+        startTime = 0;
+        endTime = 0;
+        currentLife = 0;
+        anime = GetComponent<Animator>();
+        rb2d = GetComponent<Rigidbody2D>();
+        //anime.SetTrigger("Flap");
+        //playSize = Camera.main.orthographicSize;
+
+        playSize = 2.3f + 5.5f;
+
+
+
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (startTime < 2)
+        {
+            startTime += Time.deltaTime;
+        }
+        if (startBlinking == true)
+        {
+            SpriteBlinkingEffect();
+
+        }
+
+        //Debug.Log(AppData.plutoData.angle);
+        if (!isDead && !FGC.gameOver)
+        {//
+         // 
+            if (columnHit)
+            {
+                anime.SetTrigger("Idle");
+                columnHit = false;
+            }
+            targetAngle = approxRollingAverage(targetAngle, Angle2Screen(PlutoComm.angle));
+            transform.position = new Vector2(Mathf.SmoothStep(-13, -7, startTime / 2), Mathf.Clamp(targetAngle, -2.5f, 7));
+            //rb2d./*MovePosition*/(new Vector2(-7, Angle2Screen(AppData.plutoData.angle)));
+        }
+        else if (FGC.gameOver)
+        {
+            endTime += Time.deltaTime;
+
+            float y = Mathf.Abs(2 * Mathf.Sin(2 * endTime));
+            transform.localPosition = new Vector3(transform.position.x + 3 * Time.deltaTime, transform.position.y, 0);
+        }
+
+        anime.updateMode = AnimatorUpdateMode.UnscaledTime;
+
+    }
+    float approxRollingAverage(float avg, float new_sample)
+    {
+
+        avg = avg * 0.9f + 0.1f * new_sample;
+
+        return avg;
+    }
+    public void SpriteBlinkingEffect()
+    {
+        spriteBlinkingTotalTimer += Time.deltaTime;
+        if (spriteBlinkingTotalTimer >= spriteBlinkingTotalDuration)
+        {
+            startBlinking = false;
+            spriteBlinkingTotalTimer = 0.0f;
+            this.gameObject.GetComponent<SpriteRenderer>().enabled = true;   // according to 
+                                                                             //your sprite
+            return;
+        }
+
+        spriteBlinkingTimer += Time.deltaTime;
+        if (spriteBlinkingTimer >= spriteBlinkingMiniDuration)
+        {
+            spriteBlinkingTimer = 0.0f;
+            if (this.gameObject.GetComponent<SpriteRenderer>().enabled == true)
+            {
+                this.gameObject.GetComponent<SpriteRenderer>().enabled = false;  //make changes
+            }
+            else
+            {
+                this.gameObject.GetComponent<SpriteRenderer>().enabled = true;   //make changes
+            }
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+
+        Debug.Log("Collion " + collision.gameObject.tag);
+        if (collision.gameObject.tag == "TopCollider" || collision.gameObject.tag == "BottomCollider")
+        {
+
+            startBlinking = true;
+            currentLife++;
+            life.fillAmount = ((float)currentLife / totalLife);
+            // anime.SetTrigger("Die");
+            columnHit = true;
+            if (currentLife >= totalLife)
+            {
+
+                FlappyGameControl.instance.gameduration = -1;
+                FlappyGameControl.instance.gameOver = true;
+                //FlappyGameControl.instance.BirdDied();
+                anime.SetTrigger("Die");
+                isDead = true;
+                anime.SetTrigger("Die");
+            }
+
+        }
+
+    }
+    public static float Angle2Screen(float angle)
+    {
+        MechanismData mechanismData = new MechanismData(AppData.selectedMechanism);
+        float tmin = mechanismData.tmin;
+        float tmax = mechanismData.tmax;
+        return (-2.3f + (angle - tmin) * (playSize) / (tmax - tmin));
+
+
+
+    }
+
+
+}
---
+++ Assets/FlappyBirdStyleAssets/Scripts/FlappyGameControl.cs
@@ -0,0 +1,224 @@
+﻿
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+public class FlappyGameControl : MonoBehaviour
+{
+    public AudioClip[] winClip;
+    public AudioClip[] hitClip;
+    public Text ScoreText;
+    //public ProgressBar timerObject;
+    public static FlappyGameControl instance;
+    //public RockVR.Video.VideoCapture vdc;
+    public GameObject GameOverText;
+    public bool gameOver = false;
+    public float scrollSpeed = -3f;
+    private int score;
+    public GameObject[] pauseObjects;
+    public float gameduration = 90;
+    public GameObject start;
+    int win = 0;
+    bool endValSet = false;
+
+
+    public BirdControl bc;
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != null)
+        {
+            Destroy(gameObject);
+        }
+
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        AppData.initializeStuff();
+        Time.timeScale = 1;
+       
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateGameDurationUI();
+
+        //uses the p button to pause and unpause the game
+        if (Input.GetKeyDown(KeyCode.P) )
+        {
+            if (!gameOver)
+            {
+                if (Time.timeScale == 1)
+                {
+                    Time.timeScale = 0;
+                    showPaused();
+                }
+                else if (Time.timeScale == 0)
+                {
+                    Time.timeScale = 1;
+                    hidePaused();
+                }
+            }
+            else if (gameOver)
+            {
+                hidePaused();
+                playAgain();
+            }
+        }
+
+
+
+
+    }
+
+
+    void UpdateGameDurationUI()
+    {
+       // timerObject.specifiedValue = Mathf.Clamp(100 * (90 - gameduration) / 90f, 0, 100); ;
+
+    }
+
+    //shows objects with ShowOnPause tag
+    public void showPaused()
+    {
+         foreach (GameObject g in pauseObjects)
+        {
+            g.SetActive(true);
+        }
+    }
+
+    //hides objects with ShowOnPause tag
+    public void hidePaused()
+    {
+
+        foreach (GameObject g in pauseObjects)
+        {
+            g.SetActive(false);
+        }
+    }
+    public void BirdDied()
+    {
+        endValSet = true;
+        //if (win == 1)
+        //{
+        //    GameOverText.GetComponent<Text>().text = "Great Work! \n You WON! :)";
+        //    switch (AppData.startGamePerformace)
+        //    {
+
+        //        case 2:
+        //            GameOverText.GetComponent<Text>().text = "Great Work! Level Increased! :) \n"
+        //                + "Current Level :" + AppData.endGameLevelSpeed;
+
+        //            break;
+
+        //    }
+        //}
+        if (win == -1)
+            GameOverText.GetComponent<Text>().text = "Try Again";
+        GameOverText.SetActive(true);
+        gameOver = true;
+
+    }
+    public void BirdScored()
+    {
+
+
+        if (gameduration < 0 && !endValSet)
+        {
+            //FB_spawnTargets.instance.setZero();
+            gameduration = 0;
+
+
+            if (!gameOver)
+            {
+                win = 1;
+            }
+            else
+            {
+                win = -1;
+            }
+            gameOver = true;
+            Debug.Log(win);
+            score = 0;
+            BirdDied();
+
+        }
+        else
+        {
+            if (!bc.startBlinking)
+            {
+                int index = UnityEngine.Random.Range(0, winClip.Length);
+                GetComponent<AudioSource>().clip = winClip[index];
+                if (score != 0)
+                {
+                    GetComponent<AudioSource>().Play();
+                }
+                score += 1;
+            }
+            else
+            {
+                int index = UnityEngine.Random.Range(0, hitClip.Length);
+                GetComponent<AudioSource>().clip = hitClip[index];
+                GetComponent<AudioSource>().Play();
+
+            }
+            //FB_spawnTargets.instance.reached = true;
+
+            ScoreText.text = "Score: " + score.ToString();
+            FlappyColumnPool.instance.spawnColumn();
+        }
+    }
+    public void playAgain()
+    {
+        if (gameOver == true)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+
+        }
+        if (!gameOver)
+        {
+            if (Time.timeScale == 1)
+            {
+                Time.timeScale = 0;
+                showPaused();
+            }
+            else if (Time.timeScale == 0)
+            {
+                Time.timeScale = 1;
+                hidePaused();
+            }
+
+        }
+
+    }
+    public void PlayStart()
+    {
+        endValSet = false;
+        start.SetActive(false);
+        Time.timeScale = 1;
+    }
+
+    public void continueButton()
+    {
+        if (Time.timeScale == 0)
+        {
+            Time.timeScale = 1;
+            hidePaused();
+
+        }
+    }
+
+
+}
