800f3ba5dd9950d04985080cd91bcd4ed5cf3a88
jayasurya-7 <my600yll@gmail.com>
2025-11-27 10:45:46 +0530

    NRS version

--- Assets/Games/RNR/Assets/Scripts/GameManager.cs
+++ Assets/Games/RNR/Assets/Scripts/GameManager.cs
@@ -185,7 +185,7 @@ public class GameManager : MonoBehaviour
         rainDurationToGrow = 1.0f + slope * (mechanismSpeed - 10f);
 
         // Clamp to safe range
-        rainDurationToGrow = Mathf.Clamp(rainDurationToGrow, 0.2f, 1.0f);
+        rainDurationToGrow = Mathf.Clamp(rainDurationToGrow, 0.5f, 1.0f);
         // Linear relation between speed (10 → 40) and duration (7s → 4.5s)
         float duration = -0.0833f * mechanismSpeed + 7.833f;
 
